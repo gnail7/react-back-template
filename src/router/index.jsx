cea1d2d1f5da92f948c7680d68cb5cb2b03d58dd
xuzhiliang <xuzhiliang@cubeenergy.com.cn>
2024-09-02 10:03:45 +0800

    动态路由

--- src/router/index.jsx
+++ src/router/index.jsx
@@ -1,20 +1,93 @@
 import { createBrowserRouter } from "react-router-dom"
 import App  from "../App"
 import  LazyComponent  from "./LazyComponent"
+import { useState, useEffect } from "react"
+import { useRoutes } from "react-router-dom"
+import { Navigate } from "react-router-dom"
 
-const router = createBrowserRouter([
+const redirectRoute = {
+  path: "/",
+  element: <Navigate to='/home'/>,
+}
+
+const defaultRoutes = [
   {
     path: "/",
     element: <App />,
+    children: [
+      {
+        path: "/home",
+        name: 'home',
+        element: <LazyComponent 
+          importFunc={() => import('@/pages/Home')} 
+          Fallback={<>loading....</>}
+        />,
+      }
+    ]
   },
-  {
-    path: "/home",
-    name: 'home',
-    element: <LazyComponent 
-      importFunc={() => import('../pages/home')} 
-      Fallback={<>loading....</>}
-    />,
-  }
-])
+]
 
-export default router
+// dynamic route part always get by the API
+const dynamicRoutes = [ 
+  {
+    path: "/about",
+    name: 'about',
+    filePath: '../pages/Dashboard/index.jsx'
+  }
+]
+
+
+export default function FilterRouter() {
+  const [routes, setRoutes] = useState([]);
+
+  const loadRouteElement = async (route) => {
+    return {
+      ...route,
+      element: (
+        <LazyComponent
+          importFunc={() => import(`${route.filePath}`)}
+          Fallback={<>loading....</>}
+        />
+      ),
+    };
+  };
+
+  const traverseAndLoadRoutes = async (routes) => {
+    const loadedRoutes = await Promise.all(
+      routes.map(async (route) => {
+        if (route.filePath) {
+          route = await loadRouteElement(route);
+        }
+
+        if (route.children) {
+          route.children = await traverseAndLoadRoutes(route.children);
+        }
+
+        return route;
+      })
+    );
+
+    return loadedRoutes;
+  };
+
+  useEffect(() => {
+    const initRoutes = async () => {
+      const loadedDynamicRoutes = await traverseAndLoadRoutes(dynamicRoutes);
+      const combinedRoutes = [
+        ...defaultRoutes[0].children,
+        ...loadedDynamicRoutes,
+      ];
+
+      setRoutes([
+        {
+          ...defaultRoutes[0],
+          children: [redirectRoute, ...combinedRoutes],
+        },
+      ]);
+    };
+
+    initRoutes();
+  }, [])
+
+  return <>{useRoutes(routes)}</>;
+}
